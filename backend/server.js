271994c977c1b0ee303bf4aa570a09c4915b450a
ASHFAK V.A <115928880+ASHFAK-V-A@users.noreply.github.com>
2025-10-16 01:17:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000
 
 // ✅ Configure CORS
 const allowedOrigins = [
-  "http://localhost:3000", /
+  "http://localhost:3000", 
   "https://task-management-app-dux.onrender.com", 
 ]
 
